9ee8cbfdc67b3dae90336707c7fed047f37c4d0e
Saurabh Mhase <saurabhmhaseie2@gmail.com>
2020-07-11 15:11:17 +0530

    Making Components

---
+++ 01_MakingComponents/README.md
@@ -0,0 +1,25 @@
+# Making Components 
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
+
+## @Input decorator
+
+Used to take input into an element/array from other component.
+Look at app.component and app-server.conmponent
+
+## @Output decorator
+
+Used to take emit and event give the data the other component viz parent.
+Look at app.component and cockpit.conmponent
+
+## Evemts Emitter
+
+There are various events that can be emitted to send data to parent component
+
+## Aliasing 
+
+To use different names for the elements that we are passing
+
+## Disabling Encapsulation
+
+To make css and other things apply globally
---
+++ 01_MakingComponents/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+  //This is to disable default encapsulation of css and component 
+})
+
+export class AppComponent {
+  serverElements = [{type:"server", name:"TestServer", content:"Just a test"}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+  
+  onBluePrintAdded(blueprintData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+}
+
